127c03f330103b8eaa9796b8ab06c7477ae8a0ec
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2025-01-11 18:37:04 -0500

    sets up menu options and switch case to direct to proper function

--- app.py
+++ app.py
@@ -1,15 +1,39 @@
 def add_task():
-    pass
+    print("ADD A TASK")
 
 def remove_task():
-    pass
+    print("REMOVE A TASK")
 
 def mark_task():
-    pass
+    print("MARK TASK AS COMPLETE")
 
 def see_all_task():
-    pass
+    print("LISTING ALL TASKS")
 
 
 def main():
-    pass
+
+    while True:
+        print('\n Choose an Option (1-5) from below: \n')
+        print('1: See all tasks')
+        print('2: Add task')
+        print('3: Mark task as complete')
+        print('4: Delete a task')
+        print('5: Exit app \n')
+        
+        choice = int(input())
+
+        match choice:
+            case 1:
+                see_all_task()
+            case 2:
+                add_task()
+            case 3:
+                mark_task()
+            case 4:
+                remove_task()
+            case 5:
+                break
+
+if __name__ == "__main__":
+    main()
