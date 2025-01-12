6b005e6f4f789d029b71b1771b066c93a14ee791
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2025-01-12 10:58:58 -0500

    implements add task and print all task functions

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
 def add_task():
-    print("ADD A TASK")
+    task = input('Please enter the task you would like to add: ')
+    foobar = open('tasks', 'a')
+    foobar.write(f'{task}--*!->')
+    foobar.close()
+
 
 def remove_task():
     print("REMOVE A TASK")
 
+
 def mark_task():
     print("MARK TASK AS COMPLETE")
 
-def see_all_task():
-    print("LISTING ALL TASKS")
+
+def print_all_task():
+    result_list = get_all_tasks()
+    print('\n')
+    print('<------------TASKS------------->')
+    for i in range(len(result_list) - 1):
+        print(f'{i + 1}: {result_list[i]}')
+
+        
+def get_all_tasks():
+    foobar = open('tasks', 'r+')
+    result = foobar.read()
+    return result.split('--*!->')
 
 
 def main():
-
     while True:
-        print('\n Choose an Option (1-5) from below: \n')
+        print('\n<---------Menu Options--------->')
         print('1: See all tasks')
         print('2: Add task')
         print('3: Mark task as complete')
         print('4: Delete a task')
-        print('5: Exit app \n')
+        print('5: Exit app')
         
-        choice = int(input())
+        choice = int(input('\nChoose an Option (1-5) from above: '))
 
         match choice:
             case 1:
-                see_all_task()
+                print_all_task()
             case 2:
                 add_task()
             case 3:
@@ -34,6 +49,7 @@ def main():
                 remove_task()
             case 5:
                 break
+
 
 if __name__ == "__main__":
     main()
